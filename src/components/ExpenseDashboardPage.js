2694326cf9b7a97552924b625d61dc2b75ec48b7
Kasun Kulathilaka <kasunkula@gmail.com>
2020-03-09 00:03:23 +0800

    added expense summary

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpenseSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
-    <ExpenseListFilters />
+    <ExpenseSummary />
+    <ExpenseListFilters />    
     <ExpenseList />
   </div>
 );
